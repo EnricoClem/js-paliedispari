96bd9cfc30018c3d2e7135e7d1ffa351e1ad392d
EnricoClem <enrico.clem94@gmail.com>
2024-03-12 17:45:20 +0100

    ADD - all js function and result (mi sono dimenticato di pushare il resto sorry)

--- app.js
+++ app.js
@@ -4,17 +4,45 @@
 // deve ritornare false se la parola non è palindroma
 // alla fine stampate in console un messaggio per dire all’utente il risultato del controllo
 
-console.log('Ciao mondo!')
-
 // Creare una variabile per la parola dell'utente
+const userWord = prompt ('Sai scrivere una psrola palindroma?');
+console.log(userWord)
+let confrontedWord = false;
+
 // Creare una funzione col compito di:
-    // Prendere la parola dell'utente
-    // Invertire l'ordine di lettura
+function isPalindrome() {
+
+    // Dividere la parola dell'utente e generare un array
+    let splitUserWord = userWord.split("");
+    let reversedWord = [];
+
+    // Invertire l'ordine di lettura con un ciclo che inizia la lettura dell'array dalla fine
+    for (let i = splitUserWord.length-1; i >= 0; i--) {
+        reversedWord = reversedWord.concat(splitUserWord[i]);
+    }
+
+    let joinedReversedWord = reversedWord.join('')
+
     // Confrontare la parola originale con il risultato
+    if (userWord === joinedReversedWord) {
         // Se uguale corrisponde a true
+        confrontedWord = true;
+    } else {
         // Altrimenti se diversa corrisponde a false
-// Chiedere all'utente una parola palindroma
+        confrontedWord = false;
+    };
+    
+    return confrontedWord;
+}
+
 // Evocare la funzione per analizzare la parola
+let result = isPalindrome()
+
 // Scrivere in console il risultato
+if (result == true) {
     // Se è true "La parola è palindroma"
+    console.log('La parola è palindroma')
+} else if (result == false) {
     // Altrimenti se è false "La parola non è palindroma"
+    console.log('La parola non è palindroma')
+}
