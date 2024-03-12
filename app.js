da2b8b1c1b474b5d0c4e1651246f6a2e32d79bd0
EnricoClem <enrico.clem94@gmail.com>
2024-03-12 15:58:11 +0100

    ADD - js instructio

---
+++ app.js
@@ -0,0 +1,20 @@
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma:
+// la funzione deve ritornare true se la parola è palindroma
+// deve ritornare false se la parola non è palindroma
+// alla fine stampate in console un messaggio per dire all’utente il risultato del controllo
+
+console.log('Ciao mondo!')
+
+// Creare una variabile per la parola dell'utente
+// Creare una funzione col compito di:
+    // Prendere la parola dell'utente
+    // Invertire l'ordine di lettura
+    // Confrontare la parola originale con il risultato
+        // Se uguale corrisponde a true
+        // Altrimenti se diversa corrisponde a false
+// Chiedere all'utente una parola palindroma
+// Evocare la funzione per analizzare la parola
+// Scrivere in console il risultato
+    // Se è true "La parola è palindroma"
+    // Altrimenti se è false "La parola non è palindroma"
